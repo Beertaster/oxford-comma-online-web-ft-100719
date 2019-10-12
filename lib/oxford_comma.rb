632e73e1b919a21ac9204fb785fe180a0e0a40df
William Stevens <wstevens.hornet@gmail.com>
2019-10-12 04:38:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,11 +9,10 @@
 #My first attempt I wanted to be an elegant solution but I assume didn't work well for 1-element arrays. So I took a case approach and moved on.
 
 def oxford_comma(array)
-  if array.length == 2
-  array[-2] << " and "
-  array.join
+  
   elsif array.length == 1
   	array.join
+  	
   else
     array[-1].prepend "and "
 	array.join(", ")
