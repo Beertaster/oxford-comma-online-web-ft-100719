176f0c6c211f7b75f2f2a55ad3c183ac08840be9
William Stevens <wstevens.hornet@gmail.com>
2019-10-12 04:32:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,8 @@
-def oxford_comma(array)
-  arr = []
-  array.each { |square| arr << "#{square}, " }
-  array[-1].prepend "and "
-  new_string = arr.join
-  new_string
-end
+#def oxford_comma(array)
+ # arr = []
+  #array.each { |square| arr << "#{square}, " }
+  #array[-1].prepend "and "
+  #new_string = arr.join
+  #new_string
+#end
+
